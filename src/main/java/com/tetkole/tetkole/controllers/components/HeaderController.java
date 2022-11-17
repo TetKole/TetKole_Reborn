c52204320aaa7fc015333166b9bd1d3f9256770f
blurp748 <mathistanghe@hotmail.fr>
2022-11-17 10:51:35 +0100

    [ + ] Handle linux on open folder

--- src/main/java/com/tetkole/tetkole/controllers/components/HeaderController.java
+++ src/main/java/com/tetkole/tetkole/controllers/components/HeaderController.java
@@ -19,9 +19,17 @@ public class HeaderController {
     @FXML
     public void onOpenFolderClick() throws IOException {
         String os = System.getProperty("os.name").toLowerCase();
-
+        System.out.println(os);
         if (os.contains("windows")) {
             Desktop.getDesktop().open(new File(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "\\TetKole"));
+        }else if(os.contains("linux")){
+            new Thread(() -> {
+                try {
+                    Desktop.getDesktop().open(new File("/home/" + System.getProperty("user.name") + "/TetKole/"));
+                }catch (Exception e){
+                    throw new RuntimeException(e);
+                }
+            }).start();
         }
     }
 }
