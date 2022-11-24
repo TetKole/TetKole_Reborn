5fa7a516ad4cf7f8b2b1684a789a163e49d27927
Remi Chavance <remi.chavance@orange.fr>
2022-11-24 17:34:10 +0100

    [ + ] Records List Pane + Color

--- src/main/java/com/tetkole/tetkole/controllers/AudioEditSceneController.java
+++ src/main/java/com/tetkole/tetkole/controllers/AudioEditSceneController.java
@@ -5,26 +5,31 @@ import com.tetkole.tetkole.utils.RecordManager;
 import com.tetkole.tetkole.utils.SceneManager;
 import com.tetkole.tetkole.utils.wave.WaveFormService;
 import com.tetkole.tetkole.utils.wave.WaveVisualization;
+import javafx.animation.TranslateTransition;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Screen;
 import javafx.util.Duration;
-import javafx.util.converter.LocalDateTimeStringConverter;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.net.URL;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.ResourceBundle;
@@ -47,6 +52,14 @@ public class AudioEditSceneController implements PropertyChangeListener, Initial
     private ResourceBundle resources;
     @FXML
     private HBox header;
+
+
+    @FXML
+    private Button btnDisplaySidePane;
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    private VBox vBoxPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -87,6 +100,8 @@ public class AudioEditSceneController implements PropertyChangeListener, Initial
                 mediaPlayer.seek(Duration.ZERO);
                 mediaPlayer.stop();
             });
+
+            this.settingUpSidePane();
         });
 
         waveVisualization.startVisualization(audioFile.getAbsolutePath(), WaveFormService.WaveFormJob.AMPLITUDES_AND_WAVEFORM);
@@ -151,5 +166,104 @@ public class AudioEditSceneController implements PropertyChangeListener, Initial
         double rightBorderXPosition = waveVisualization.getRightBorderXPosition() + 10;
         double newStop = rightBorderXPosition / waveVisualization.getRatioAudio() + waveVisualization.getBeginAudio();
         mediaPlayer.setStopTime(new Duration((newStop) * 1000));
+    }
+
+    private void settingUpSidePane() {
+        // test values */
+        String[] values = {
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08",
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08",
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08",
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08",
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08",
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08",
+                "record-1-14.15",
+                "record-2-16.17",
+                "record-3-19.45",
+                "record-4-20.52",
+                "record-5-34.08"
+        };
+        //*************/
+
+        vBoxPane.setSpacing(50);
+
+
+        for(String value : values) {
+            // prepare the HBox
+            HBox line = new HBox();
+            line.setAlignment(Pos.CENTER);
+            line.setSpacing(50);
+
+            // add the DeleteButton
+            Button btnDelete = new Button("Delete");
+            btnDelete.getStyleClass().add("buttons");
+            btnDelete.getStyleClass().add("blue");
+            line.getChildren().add(btnDelete);
+
+            // add the Label
+            Label label = new Label(value);
+            label.setStyle("-fx-text-fill: white;");
+            line.getChildren().add(label);
+
+            // add the Play/Pause Button
+            Button btnPlayPause = new Button("Play");
+            btnPlayPause.getStyleClass().add("buttons");
+            btnPlayPause.getStyleClass().add("blue");
+            line.getChildren().add(btnPlayPause);
+
+            // add the HBox to the VBox
+            vBoxPane.getChildren().add(line);
+        }
+
+
+
+        ChangeListener listener = (observable, oldValue, newValue) -> pane.setTranslateX(-pane.getWidth());
+
+        pane.widthProperty().addListener(listener);
+        pane.setPrefWidth(Screen.getPrimary().getBounds().getWidth() / 4);
+
+
+
+        // animated transitions
+        TranslateTransition openNav = new TranslateTransition(new Duration(350), pane);
+        openNav.setToX(0);
+        TranslateTransition closeNav = new TranslateTransition(new Duration(350), pane);
+
+        // on button click
+        btnDisplaySidePane.setOnAction(e -> {
+            if (pane.getTranslateX() != 0) {
+                openNav.play();
+            } else {
+                // delete listener on the first close (called every times java doesn't cares)
+                pane.widthProperty().removeListener(listener);
+
+                closeNav.setToX(-(pane.getWidth()));
+                closeNav.play();
+            }
+        });
     }
 }
