ccfef7f39de3f3df95a592293e5f1265257e3820
mathis <mathistanghe@hotmail.fr>
2023-01-04 14:38:16 +0100

    [ + ] Create Tetkole folder

--- src/main/java/com/tetkole/tetkole/utils/FileManager.java
+++ src/main/java/com/tetkole/tetkole/utils/FileManager.java
@@ -29,6 +29,7 @@ public class FileManager {
             this.isWindows = false;
         } else {
             this.folderPath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "\\TetKole";
+            new File(this.folderPath).mkdir();
             this.isWindows = true;
         }
     }
